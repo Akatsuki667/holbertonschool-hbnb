5eb567b1b7da73ae3694770e936878a8f95336a8
Akatsuki667 <qextzy@gmail.com>
2025-04-24 16:33:59 +0200

    task 0 completed, mise en place task1

---
+++ part4/login.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+          // Your code to handle form submission
+          
+        });
+    }
+});

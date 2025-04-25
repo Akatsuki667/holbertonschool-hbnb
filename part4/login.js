a2462ce6cc29f63a0c77df204443658278383951
Akatsuki667 <qextzy@gmail.com>
2025-04-25 11:01:09 +0200

    task 1 completed

--- part4/login.js
+++ part4/login.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('login-form');
+  const loginForm = document.getElementById('login-form');
 
-    if (loginForm) {
-        loginForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-          // Your code to handle form submission
-          
-        });
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://127.0.0.1:5000/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: `${loginForm.email.value}`,
+        password: `${loginForm.password.value}`
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      document.cookie = `token=${data.access_token}; path=/`;
+      window.location.href = 'index.html';
+    } else {
+      alert('Login failed: ' + response.statusText);
     }
+  });
 });
